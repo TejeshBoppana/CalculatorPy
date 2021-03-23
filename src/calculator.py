fd297ee5cfccf0d7aeaf3a24e5d805e57b160ead
TejeshBoppana <tb334@njit.edu>
2021-03-23 11:21:52 -0400

    Task: Created Test Module

---
+++ src/calculator.py
@@ -0,0 +1,7 @@
+import math
+
+def addition(a,b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
---
+++ src/calculatorTests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from calculator import calculator
+from csvreader import csvreader
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, calculator)
+
+    def test_addition(self):
+        test_data = csvreader('src/csv/UnitTestAddition.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.addition(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
