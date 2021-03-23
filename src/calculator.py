385d94d5ab2793e67071f88358f330cf216013c8
TejeshBoppana <tb334@njit.edu>
2021-03-23 12:16:52 -0400

    Fix: Cleared Errors In Divison File

--- src/calculator.py
+++ src/calculator.py
@@ -27,3 +27,32 @@ def sqrt(a):
     a = float(a)
     c = math.sqrt(a)
     return c
+class calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def addition(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtraction(self, a, b):
+        self.result = sub(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = times(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = div(a, b)
+        return self.result
+
+    def square_(self, a):
+        self.result = square(a)
+        return self.result
+
+    def sqrt_(self, a):
+        self.result = sqrt(a)
+        return self.result
